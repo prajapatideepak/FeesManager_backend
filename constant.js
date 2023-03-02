e6e2d437c58947d646c450a032865fcb3d9d0ac4
Deepak Prajapati <prajapatideepak073@gmail.com>
2023-03-02 17:30:22 +0530

    constant

---
+++ constant.js
@@ -0,0 +1,23 @@
+module.exports = {
+    //testing db
+    MONGODB_URL : "mongodb+srv://wellbenix:kFcNsSsEyPJxKAhg@cluster0.c6nogfk.mongodb.net/fees_manager?retryWrites=true&w=majority",
+
+    //nasir serverless db
+    // MONGODB_URL : "mongodb+srv://nasir:KAIO1AZme6mzSxgo@nasir.vxcap.mongodb.net/?retryWrites=true&w=majority",
+
+    JWT_SIGN : "pyarmedardhimiltahe",
+
+    USER : "nasirsirclasses1@gmail.com",
+    PASSWORD : "kuqrmptbzltppeuw",
+
+    // My credentials
+    IMAGEKIT_URL_ENDPOINT : "https://ik.imagekit.io/cpxw6dfc",
+    IMAGEKIT_PUBLIC_KEY : "public_o8LSXDAWmMqHb6cVykfiMWHWs2o=",
+    IMAGEKIT_PRIVATE_KEY : "private_7fu6h5BhRAhY04GaiozAmGm9UoY="
+
+    // Nasir credentials
+
+    // IMAGEKIT_URL_ENDPOINT : "https://ik.imagekit.io/44qikvq89",
+    // IMAGEKIT_PUBLIC_KEY : "public_iTX7TFUEBScLfYySQgaYxe7yj2U=",
+    // IMAGEKIT_PRIVATE_KEY : "private_IM+Uvul930B8O6TbGuJtMhDwpg8="
+}
